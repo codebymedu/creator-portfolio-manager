0f08f25c22a962388bf09009eb79856d79bd86f4
Medu <03medu@gmail.com>
2024-05-26 00:34:03 +0200

    fix: hydration problem

--- app/(landing)/_components/section/process.tsx
+++ app/(landing)/_components/section/process.tsx
@@ -15,23 +15,28 @@ export const ProcessSection = () => (
     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-3">
       {processSteps.map((step) => (
         <div key={step.name}>
-          <p className="flex items-center text-sm font-semibold leading-6 text-neutral-600">
-            <svg
-              viewBox="0 0 4 4"
-              className="mr-4 h-1 w-1 flex-none"
-              aria-hidden="true"
-            >
-              <circle cx={2} cy={2} r={2} fill="currentColor" />
-            </svg>
-            {step.countLabel}
+          <div className="flex gap-2 flex-row items-center">
+            <p className="flex items-center text-sm font-semibold leading-6 text-neutral-600">
+              <svg
+                viewBox="0 0 4 4"
+                className="mr-4 h-1 w-1 flex-none"
+                aria-hidden="true"
+              >
+                <circle cx={2} cy={2} r={2} fill="currentColor" />
+              </svg>
+              {step.countLabel}
+            </p>
+
             <div
               className="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
               aria-hidden="true"
             />
-          </p>
+          </div>
+
           <p className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
             {step.name}
           </p>
+
           <p className="mt-1 text-base leading-7 text-gray-600">
             {step.description}
           </p>
